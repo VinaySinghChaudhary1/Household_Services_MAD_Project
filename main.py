96ecf2546be17cce0ec06f45ff88f0c76a088ef1
VinaySinghChaudhary1 <22f2001153@ds.study.iitm.ac.in>
2024-10-03 11:16:12 +0530

    add template

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/about/<user_name')
+def about(user_name):
+    return render_template('about.html', name=user_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
