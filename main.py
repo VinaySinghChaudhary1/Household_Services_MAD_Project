3426b285dd6cb08ec393e5a32858cde85d6c2f39
VinaySinghChaudhary1 <22f2001153@ds.study.iitm.ac.in>
2024-10-05 08:50:34 +0530

    repairing code

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
-@app.route('/about/<user_name')
+@app.route('/about/<user_name>')
 def about(user_name):
     return render_template('about.html', name=user_name)
 
+# @app.route('/contact')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register_customer')
+def register_customer():
+    return render_template('register_customer.html')
+
+
+@app.route('/register_supplier')
+def register_supplier():
+    return render_template('register_supplier.html')
+        
 if __name__ == '__main__':
     app.run(debug=True)
